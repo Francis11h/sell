b9b229298eb814c9ce20f823beae472bb68d0517
Francis11h <zhan23@ncsu.edu>
2020-04-04 18:44:44 -0400

    Volatile 保证可见性

---
+++ src/test/java/com/imooc/sell/lilunyanzheng/VolatileDemo.java
@@ -0,0 +1,56 @@
+package com.imooc.sell.lilunyanzheng;
+
+
+import java.util.concurrent.TimeUnit;
+
+class MyData {
+    volatile int number = 0;
+    public void addTo60() {
+        this.number = 60;
+    }
+}
+
+class MyThread implements Runnable {
+    public MyData mydata;
+
+    public MyThread (MyData myData) {
+        this.mydata = myData;
+    }
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName() + "\t come in");
+        try {
+            TimeUnit.SECONDS.sleep(3);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        mydata.addTo60();
+        System.out.println(Thread.currentThread().getName() + "\t update number value: " + mydata.number);
+    }
+}
+
+/**
+ * 验证Volatile的可见性
+ * 1.1 假设number变量为0 之前number 变量没有添加 volatile修饰
+ *
+ */
+public class VolatileDemo {
+
+    public static void main(String[] args) {
+        MyData myData = new MyData();   //线程操纵资源类
+
+//        new Thread(() -> {
+//
+//        }, "AAA").start();
+        // 如果不在外头包一个 new Thread().start() 则不会新建新的线程 相当于还是 主main线程 不信可以实验 但是这个是 用接口创建 直接extends Thread 就可以直接启
+        new Thread(new MyThread(myData), "AAA").start();
+
+
+        while (myData.number == 0) {
+            // main 进程一直等待循环 直到number的值不再等于0
+        }
+
+        System.out.println(Thread.currentThread().getName() + "\t mission is over");
+    }
+
+}
